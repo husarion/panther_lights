1e050160aa356a4866984cb384607fb3e01d657f
byq77 <byq77@hotmail.com>
2020-10-16 15:27:10 +0200

    fixed and new animations, added brightness control

--- src/panther_lights/driver.py
+++ src/panther_lights/driver.py
@@ -8,7 +8,6 @@ import time
 from queue import Queue
 
 class Animation:
- 
     class Type(Enum):
         NO_ANIMATION = 0
         FADE_COLOR = 1
@@ -21,15 +20,16 @@ class Animation:
         COLOR_WIPE_DOWN_SLOW = 8
         COLOR_WIPE_HALF_UP_FAST = 9
         COLOR_WIPE_HALF_DOWN_FAST = 10
-        DOUBLE_COLOR_WIPE_NORMAL = 11
-        DOUBLE_COLOR_WIPE_FAST = 12
-
+        ONE_DOUBLE_COLOR_WIPE_NORMAL = 11
+        ONE_DOUBLE_COLOR_WIPE_FAST = 12
+        DOUBLE_COLOR_WIPE_NORMAL = 13
+        DOUBLE_COLOR_WIPE_FAST = 14
 
     def __init__(self, num_pixel, default_animation=Type.NO_ANIMATION.value, default_color=0):
         self.num_pixel = num_pixel
         self.buffer = [0] * num_pixel
         self.frame_count = 0
-        self.bright_percent = 100 
+        self.bright_percent = 100
         self.current_animation = default_animation
         self.animation_color = default_color
 
@@ -37,36 +37,40 @@ class Animation:
         self.fade_steps_per_cycle = 30
         self.snake_index = 0
         self.snake_wait = 6
-        self.double_snake_index = [0,0]
-    
+        self.double_snake_index = [0, 0]
+
     def update(self):
         # compute new animation frame
         if self.current_animation is None:
             return False
         if self.current_animation == Animation.Type.FADE_COLOR.value:
-            return self.fade_color()  
+            return self.fade_color()
         elif self.current_animation == Animation.Type.SOLID_COLOR.value:
             return self.solid_color()
         elif self.current_animation == Animation.Type.COLOR_WIPE_UP_SLOW.value:
-            return self.snake(True,3,False)
+            return self.snake(True, 3, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_UP_NORMAL.value:
-            return self.snake(True,1,True, False)
+            return self.snake(True, 1, True, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_UP_FAST.value:
-            return self.snake(True,3,True, False)
+            return self.snake(True, 3, True, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_DOWN_SLOW.value:
-            return self.snake(False,3,False, False)
+            return self.snake(False, 3, False, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_DOWN_NORMAL.value:
-            return self.snake(False,1,True, False)
+            return self.snake(False, 1, True, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_DOWN_FAST.value:
-            return self.snake(False,3,True, False)
+            return self.snake(False, 3, True, False)
         elif self.current_animation == Animation.Type.COLOR_WIPE_HALF_DOWN_FAST.value:
-            return self.snake(False,2,True, True)
+            return self.snake(False, 2, True, True)
         elif self.current_animation == Animation.Type.COLOR_WIPE_HALF_UP_FAST.value:
-            return self.snake(True,2,True, True)
-        elif self.current_animation == Animation.Type.DOUBLE_COLOR_WIPE_NORMAL.value:
+            return self.snake(True, 2, True, True)
+        elif self.current_animation == Animation.Type.ONE_DOUBLE_COLOR_WIPE_NORMAL.value:
             return self.double_snake(2)
-        elif self.current_animation == Animation.Type.DOUBLE_COLOR_WIPE_FAST.value:
+        elif self.current_animation == Animation.Type.ONE_DOUBLE_COLOR_WIPE_FAST.value:
             return self.double_snake(3)
+        elif self.current_animation == Animation.Type.DOUBLE_COLOR_WIPE_NORMAL.value:
+            return self.double_snake(2, False)
+        elif self.current_animation == Animation.Type.DOUBLE_COLOR_WIPE_FAST.value:
+            return self.double_snake(3, False)
 
     def fade_color(self):
         frame = self.frame_count % self.fade_steps_per_cycle
@@ -102,16 +106,16 @@ class Animation:
     def snake(self, direction, speed, mode, half=False):
         if self.frame_count == 0:
             if half:
-                self.snake_index = self.num_pixel // 2 - 1 # delta
-            else: 
-                self.snake_index = 0 if direction else self.num_pixel-1 # delta 
+                self.snake_index = self.num_pixel // 2 - 1  # delta
+            else:
+                self.snake_index = 0 if direction else self.num_pixel - 1  # delta
             for i in range(self.num_pixel):
                 self.buffer[i] = 0
 
         if direction:
-            if self.snake_index < self.num_pixel: 
+            if self.snake_index < self.num_pixel:
                 if mode:
-                    end_index = self.num_pixel if self.snake_index + speed > self.num_pixel else self.snake_index + speed 
+                    end_index = self.num_pixel if self.snake_index + speed > self.num_pixel else self.snake_index + speed
                     for i in range(self.snake_index, end_index):
                         self.buffer[i] = self.animation_color
                     self.snake_index = end_index
@@ -122,8 +126,8 @@ class Animation:
                 self.snake_index += 1
             elif self.snake_index - self.num_pixel == self.snake_wait:
                 for i in range(self.num_pixel):
-                    self.buffer[i] = 0   
-                self.snake_index += 1         
+                    self.buffer[i] = 0
+                self.snake_index += 1
             elif self.snake_index - self.num_pixel < 2 * self.snake_wait:
                 self.snake_index += 1
             else:
@@ -131,20 +135,20 @@ class Animation:
         else:
             if self.snake_index >= 0:
                 if mode:
-                    end_index = -1 if self.snake_index - speed < 0 else self.snake_index - speed 
+                    end_index = -1 if self.snake_index - speed < 0 else self.snake_index - speed
                     for i in range(self.snake_index, end_index, -1):
                         self.buffer[i] = self.animation_color
                     self.snake_index = end_index
                 elif self.frame_count % speed == 0:
                     self.buffer[self.snake_index] = self.animation_color
                     self.snake_index -= 1
-            elif self.snake_index >= - self.snake_wait-1:
+            elif self.snake_index > - self.snake_wait - 1:
                 self.snake_index -= 1
-            elif self.snake_index == -self.snake_wait-2:
+            elif self.snake_index == -self.snake_wait - 1:
                 for i in range(self.num_pixel):
                     self.buffer[i] = 0
                 self.snake_index -= 1
-            elif self.snake_index > (-self.snake_wait-1) * 2:
+            elif self.snake_index > (-self.snake_wait) * 2 - 1:
                 self.snake_index -= 1
             else:
                 self.snake_index = self.num_pixel // 2 - 1 if half else self.num_pixel - 1
@@ -152,16 +156,17 @@ class Animation:
         self.frame_count += 1
         return True
 
-    def double_snake(self, speed):
+    def double_snake(self, speed, once=True):
         update_frame = False
-        if self.frame_count == 0:
+        if (self.frame_count == 0) or \
+           (not once and (self.double_snake_index[1] >= self.num_pixel + 16)):
             for i in range(self.num_pixel):
                 self.buffer[i] = 0
             self.bright_percent = 70
             if self.num_pixel % 2 == 1:
                 for i in range(2):
                     self.double_snake_index[i] = self.num_pixel // 2 - 1
-                self.buffer[self.double_snake_index[0]] = self.animation_color 
+                self.buffer[self.double_snake_index[0]] = self.animation_color
             else:
                 self.double_snake_index[0] = self.num_pixel // 2 - 1
                 self.double_snake_index[1] = self.num_pixel // 2
@@ -176,10 +181,10 @@ class Animation:
             tmp = self.double_snake_index[1] + speed
             self.double_snake_index[1] = tmp if tmp < self.num_pixel else self.num_pixel
             update_frame = True
-        elif self.double_snake_index[1] < self.num_pixel + 3:
+        elif self.double_snake_index[1] < self.num_pixel + 16:
             self.bright_percent += 10
-            self.double_snake_index[1] += 1
-            for i in range(0,self.num_pixel):
+            self.double_snake_index[1] += speed
+            for i in range(0, self.num_pixel):
                 self.buffer[i] = self.animation_color
             update_frame = True
         self.frame_count += 1
@@ -187,13 +192,17 @@ class Animation:
 
 class PantherLights(Thread):
     
-    class Message:
+    class AnimationMessage:
         def __init__(self, anim_type_front, anim_color_front, anim_type_rear, anim_color_rear):
             self.anim_type_front = anim_type_front
             self.anim_type_rear = anim_type_rear
             self.anim_color_front = anim_color_front
             self.anim_color_rear = anim_color_rear
    
+    class BrightnessMessage:
+        def __init__(self, brightness: float):
+            self.brightness = brightness
+
     DELTA_TIME = 0.030
     LED_SWITCH_FRONT_STATE = True 
     LED_POWER_ON_STATE = False # active with low state 
@@ -242,20 +251,26 @@ class PantherLights(Thread):
             if not self.__queue.empty():
                 new_msg = self.__queue.get(block=False)
 
-                if new_msg.anim_type_front is not None and new_msg.anim_type_front in Animation.Type.__members__.keys():
-                    self.__animation_front.current_animation = Animation.Type[new_msg.anim_type_front].value
-                else:
-                    self.__animation_front.current_animation = None
+                if isinstance(new_msg, PantherLights.AnimationMessage):
 
-                if new_msg.anim_type_rear is not None and new_msg.anim_type_rear in Animation.Type.__members__.keys():
-                    self.__animation_rear.current_animation = Animation.Type[new_msg.anim_type_rear].value
-                else:
-                    self.__animation_rear.current_animation = None
+                    if new_msg.anim_type_front is not None and new_msg.anim_type_front in Animation.Type.__members__.keys():
+                        self.__animation_front.current_animation = Animation.Type[new_msg.anim_type_front].value
+                    else:
+                        self.__animation_front.current_animation = None
+
+                    if new_msg.anim_type_rear is not None and new_msg.anim_type_rear in Animation.Type.__members__.keys():
+                        self.__animation_rear.current_animation = Animation.Type[new_msg.anim_type_rear].value
+                    else:
+                        self.__animation_rear.current_animation = None
     
-                self.__animation_front.animation_color = new_msg.anim_color_front
-                self.__animation_rear.animation_color = new_msg.anim_color_rear
-                self.__animation_front.frame_count = 0
-                self.__animation_rear.frame_count = 0
+                    self.__animation_front.animation_color = new_msg.anim_color_front
+                    self.__animation_rear.animation_color = new_msg.anim_color_rear
+                    self.__animation_front.frame_count = 0
+                    self.__animation_rear.frame_count = 0
+
+                elif isinstance(new_msg, PantherLights.BrightnessMessage):
+
+                    self.__pixels.global_brightness = min(int(new_msg.brightness * 31), 31)
 
             if new_msg is not None:
                 del new_msg
@@ -280,32 +295,3 @@ class PantherLights(Thread):
         self.__pixels.cleanup()
         GPIO.output(self.__led_power_pin, not PantherLights.LED_POWER_ON_STATE) 
         # GPIO.cleanup()
-
-
-def main():
-    format = "%(asctime)s: %(message)s"
-    stop_thread_event = Event()
-    stop_thread_event.clear()
-    my_queue = Queue(10)
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    lights = PantherLights(event=stop_thread_event, queue=my_queue)
-    lights.start()
-    
-    while 1:
-        try:
-            my_queue.put(PantherLights.Message('FADE_COLOR', 0xEEDD00, 'FADE_COLOR', 0xEEDD00,))
-            time.sleep(4)
-            my_queue.put(PantherLights.Message('COLOR_WIPE_HALF_DOWN_FAST', 0xff6600, 'COLOR_WIPE_HALF_UP_FAST', 0xff6600,))
-            time.sleep(4)
-            my_queue.put(PantherLights.Message('COLOR_WIPE_HALF_UP_FAST', 0xff6600, 'COLOR_WIPE_HALF_DOWN_FAST', 0xff6600,))
-            time.sleep(4)
-            my_queue.put(PantherLights.Message('DOUBLE_COLOR_WIPE_FAST', 0xff0000, 'SOLID_COLOR', 0xff0000))
-            time.sleep(4)
-        except KeyboardInterrupt:
-            stop_thread_event.set()
-            lights.join()
-            break
-    logging.info("Closing program...")
-
-if __name__=="__main__":
-    main()
